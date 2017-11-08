b209bd3a1e6c12955bd579d04631fe9df7547ebc
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-08 12:15:33 +0000

    added models restaurant and review  and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [ :new, :create, :index ]
+   #  new_restaurant_review  GET   /restaurants/:restaurant_id/reviews/new  reviews#new
+   # restaurant_reviews  POST  /restaurants/:restaurant_id/reviews      reviews#create
+  end
+
 end
+
+
+# restaurants
+# index, new, show,
+# review
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '01 01 09 90 79',
+    category:  'chinese',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '11 33 09 00 79',
+    category:  'italian',
+  },
+  {
+    name:         'Pizza Hut',
+    address:      '10 Killburn High St, London N2 6PQ',
+    phone_number: '13 53 19 30 79',
+    category:  'belgian',
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
